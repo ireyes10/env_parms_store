0f42814913e4ae8fc27eb0424a6b8caac06b5004
ireyes 10 <ireyes@infile.com.gt>
2023-07-05 08:30:41 -0600

    add params load_app

--- lib/env_params/engine.rb
+++ lib/env_params/engine.rb
@@ -2,10 +2,14 @@ require "rails"
 
 module EnvParams
   class Engine < Rails::Engine
-   
 
+    isolate_namespace EnvParams
+
+    initializer 'my_plugin.routes' do
+      EnvParams::Engine.routes.draw do
+        get '/custom_route', to: 'my_controller#my_action'
+      end
+    end
 
   end
-
-
 end
--- lib/env_params/railtie.rb
+++ lib/env_params/railtie.rb
@@ -2,9 +2,6 @@ module EnvParams
   class Railtie < ::Rails::Railtie
     
 
-    initializer 'env_params.load_app' do |app|
-      option.load_app = app.config.env_params.load_app
-    end
 
   end
 end
