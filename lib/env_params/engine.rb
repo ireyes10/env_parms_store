eb7ca97302035268ff23a63783ab2b892ef4f7b1
ireyes 10 <ireyes@infile.com.gt>
2023-07-04 18:50:55 -0600

    add params load_app

--- lib/env_params/engine.rb
+++ lib/env_params/engine.rb
@@ -1,6 +1,10 @@
+require "rails"
+
 module EnvParams
   class Engine < Rails::Engine
-    
+    #config.env_params.load_app = 
+    config.env_params.load_app = ActiveSupport::InheritableOptions.new
+    config.eager_load_namespaces << EnvParams
 
     initializer "env_params.configs" do
       config.after_initialize do |app|
